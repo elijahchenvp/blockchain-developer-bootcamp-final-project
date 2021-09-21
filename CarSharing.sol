7435cb37c5a732ebc88392fce171f45d5cd493a6
ELIJAH CHEN <elijahchen@ELIJAHs-iMac.local>
2021-09-21 23:10:13 +0800

    Committer: ELIJAH CHEN <elijahchen@ELIJAHs-iMac.local>
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   CarSharing.sol

---
+++ CarSharing.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.16 <0.9.0;
+
+contract CarSharing {
+
+modifier onlyRentOne() { 
+    
+	// checks if driver is currently renting other car
+	_ 
+};
+
+function registerDriver(address _driver) {
+
+	// registers driver
+
+};
+
+function unrentCar(address _driver) {
+
+	// finish rental of car and release the reservation slot
+
+};
+
+function rentCar(uint carID) {
+
+	// reserves and start rental of car
+
+};
+
+}
