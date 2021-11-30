22c2dceaf67bd01d3b4b1dbe8fbe3856a3f89bd7
ELIJAH CHEN <elijahchen@ELIJAHs-iMac.local>
2021-11-30 16:20:00 +0800

    30 nov 2021

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-//var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var CarSharing = artifacts.require("./CarSharing.sol");
-
-module.exports = function(deployer) {
-  //deployer.deploy(SimpleStorage);
-  deployer.deploy(CarSharing);
-};
